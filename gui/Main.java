2dd98a131fe6687ae8617dff1c9dde6492314154
Markus <markus.reynoso.c@gmail.com>
2024-08-08 13:06:42 +0800

    Finish map maker ui

--- gui/Main.java
+++ gui/Main.java
@@ -1,6 +1,5 @@
 package gui;
 
-import javax.lang.model.type.ArrayType;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -15,14 +14,14 @@ public class Main {
         JFrame frame = new JFrame();
         JPanel gridPanel = new JPanel();
         JPanel rightPanel = new JPanel();
-        JButton playerButton = new JButton("Player");
-        JButton playerOnGoalButton = new JButton("Player on Goal");
-        JButton wallButton = new JButton("Wall");
-        JButton boxButton = new JButton("Box");
-        JButton greenBoxButton = new JButton("Green Box");
-        JButton goalButton = new JButton("Goal");
-        JButton delButton = new JButton("Delete");
-        JButton startButton = new JButton("Start");
+        JButton playerButton = new JButton("<html><div style='text-align: center;'>Player</div></html>");
+        JButton playerOnGoalButton = new JButton("<html><div style='text-align: center;'>Player on Goal</div></html>");
+        JButton wallButton = new JButton("<html><div style='text-align: center;'>Wall</div></html>");
+        JButton boxButton = new JButton("<html><div style='text-align: center;'>Box</div></html>");
+        JButton greenBoxButton = new JButton("<html><div style='text-align: center;'>Box on Goal</div></html>");
+        JButton goalButton = new JButton("<html><div style='text-align: center;'>Goal</div></html>");
+        JButton delButton = new JButton("<html><div style='text-align: center;'>Delete</div></html>");
+        JButton startButton = new JButton("<html><div style='text-align: center;'>Start</div></html>");
 
         for (int i = 0; i < 25; i++){
             for (int j = 0; j < 25; j++){
@@ -35,15 +34,18 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+        frame.setBackground(new Color(0xf0f0f0));
 
         gridPanel.setBackground(Color.black);
         gridPanel.setSize(500,500);
         gridPanel.setLayout(new GridLayout(25,25, 0,0));
+        gridPanel.setBackground(new Color(0xf0f0f0));
 
         for (int i = 0; i <= 24; i++){
             for (int j = 0; j <= 24; j++){
                 JButton button = new JButton();
-                button.setBackground(Color.black);
+                button.setBorder(BorderFactory.createEmptyBorder());
+                button.setBackground(new Color(0xf0f0f0));
                 button.addActionListener(new GridButton(this, i, j));
                 gridPanel.add(button);
             }
@@ -56,43 +58,82 @@ public class Main {
         Dimension buttonSize = new Dimension(160, 80); // Set a fixed size for the buttons
 
         playerButton.setPreferredSize(buttonSize);
+        playerButton.setBackground(new Color(0x072448));
+        playerButton.setForeground(new Color(0xf0f0f0));
+        playerButton.setHorizontalAlignment(SwingConstants.CENTER);
+        playerButton.setVerticalAlignment(SwingConstants.CENTER);
+        playerButton.setMargin(new Insets(10, 10, 10, 10));
         playerButton.addActionListener(new ComponentButton(this, '@'));
         rightPanel.add(playerButton);
 
         playerOnGoalButton.setPreferredSize(buttonSize);
+        playerOnGoalButton.setBackground(new Color(0x54d2d2));
+        playerOnGoalButton.setForeground(new Color(0xf0f0f0));
+        playerOnGoalButton.setHorizontalAlignment(SwingConstants.CENTER);
+        playerOnGoalButton.setVerticalAlignment(SwingConstants.CENTER);
+        playerOnGoalButton.setMargin(new Insets(10, 10, 10, 10));
         playerOnGoalButton.addActionListener(new ComponentButton(this, '+'));
         rightPanel.add(playerOnGoalButton);
 
         wallButton.setPreferredSize(buttonSize);
+        wallButton.setBackground(new Color(0xff6150));
+        wallButton.setForeground(new Color(0xf0f0f0));
+        wallButton.setHorizontalAlignment(SwingConstants.CENTER);
+        wallButton.setVerticalAlignment(SwingConstants.CENTER);
+        wallButton.setMargin(new Insets(10, 10, 10, 10));
         wallButton.addActionListener(new ComponentButton(this, '#'));
         rightPanel.add(wallButton);
 
         boxButton.setPreferredSize(buttonSize);
+        boxButton.setBackground(new Color(0xf8aa4b));
+        boxButton.setForeground(new Color(0xf0f0f0));
+        boxButton.setHorizontalAlignment(SwingConstants.CENTER);
+        boxButton.setVerticalAlignment(SwingConstants.CENTER);
+        boxButton.setMargin(new Insets(10, 10, 10, 10));
         boxButton.addActionListener(new ComponentButton(this, '$'));
         rightPanel.add(boxButton);
 
         greenBoxButton.setPreferredSize(buttonSize);
+        greenBoxButton.setBackground(new Color(0x54d2a0));
+        greenBoxButton.setForeground(new Color(0xf0f0f0));
+        greenBoxButton.setHorizontalAlignment(SwingConstants.CENTER);
+        greenBoxButton.setVerticalAlignment(SwingConstants.CENTER);
+        greenBoxButton.setMargin(new Insets(10, 10, 10, 10));
         greenBoxButton.addActionListener(new ComponentButton(this, '*'));
         rightPanel.add(greenBoxButton);
 
         goalButton.setPreferredSize(buttonSize);
+        goalButton.setBackground(new Color(0xffcb00));
+        goalButton.setForeground(new Color(0xf0f0f0));
+        goalButton.setHorizontalAlignment(SwingConstants.CENTER);
+        goalButton.setVerticalAlignment(SwingConstants.CENTER);
+        goalButton.setMargin(new Insets(10, 10, 10, 10));
         goalButton.addActionListener(new ComponentButton(this, '.'));
         rightPanel.add(goalButton);
 
         delButton.setPreferredSize(buttonSize);
+        delButton.setBackground(new Color(0x2E2E2E));
+        delButton.setForeground(new Color(0xf0f0f0));
+        delButton.setHorizontalAlignment(SwingConstants.CENTER);
+        delButton.setVerticalAlignment(SwingConstants.CENTER);
+        delButton.setMargin(new Insets(10, 10, 10, 10));
         delButton.addActionListener(new ComponentButton(this, ' '));
         rightPanel.add(delButton);
 
         startButton.setPreferredSize(buttonSize);
+        startButton.setBackground(new Color(0x2E2E2E));
+        startButton.setForeground(new Color(0xf0f0f0));
+        startButton.setHorizontalAlignment(SwingConstants.CENTER);
+        startButton.setVerticalAlignment(SwingConstants.CENTER);
+        startButton.setMargin(new Insets(10, 10, 10, 10));
         startButton.addActionListener(new StartButton(this));
-        rightPanel.add(startButton);
-
         rightPanel.add(startButton);
 
         frame.add(gridPanel, BorderLayout.CENTER);
         frame.add(rightPanel, BorderLayout.EAST);
         frame.setVisible(true);
     }
+
 
     public static class GridButton implements ActionListener {
         private final int row;
@@ -108,6 +149,32 @@ public class Main {
         @Override
         public void actionPerformed(ActionEvent e) {
             this.main.map[row][col] = this.main.active;
+            Object source = e.getSource();
+
+            switch (this.main.active){
+                case '@': // Player
+                    ((JButton) source).setBackground(new Color(0x072448));
+                    break;
+                case '+': // Player on goal
+                    ((JButton) source).setBackground(new Color(0x54d2d2));
+                    break;
+                case '#': // Wall
+                    ((JButton) source).setBackground(new Color(0xff6150));
+                    break;
+                case '$': // Box
+                    ((JButton) source).setBackground(new Color(0xf8aa4b));
+                    break;
+                case '*': // Box on goal
+                    ((JButton) source).setBackground(new Color(0x54d2a0));
+                    break;
+                case '.': // Goal
+                    ((JButton) source).setBackground(new Color(0xffcb00));
+                    break;
+                case ' ':
+                    ((JButton) source).setBackground(new Color(0xf0f0f0));;
+                    break;
+            }
+
         }
     }
 
@@ -135,6 +202,7 @@ public class Main {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            // ToDO
             for (char[] row: this.main.map){
                 System.out.println(Arrays.toString(row));
             }
